68c0ee2f8432911e51aff44467296e47b9ab2f2a
GuaBaiChi <87482626+GuaBaiChi@users.noreply.github.com>
2023-01-10 14:35:54 -0500

    2023.01.10
    Up to part 01-10

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,8 @@
 # Write your solution here
+name = input("Given name: ")
+fname = input("Family name: ")
+addy = input("Street address: ")
+city = input("City and postal code: ")
+print(name + " " + fname)
+print(addy)
+print(city)
